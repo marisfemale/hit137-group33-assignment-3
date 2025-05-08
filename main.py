bb780a89eb36032f6c636ca9e736e6e3c751ee08
MARIS\maris <maris.female@gmail.com>
2025-05-08 12:50:19 +0930

    image show on matplolib window

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import tkinter as tk
+from tkinter import filedialog
 import configs as configs
+import cv2
+import matplotlib.pyplot as plt
+
+
 print(dir(configs))
 root = tk.Tk()
 
@@ -10,7 +15,15 @@ root.geometry(configs.window_size)
 
 #button open action
 def button_open_clicked():
-    print('hi')
+    file_path = tk.filedialog.askopenfilename()
+    #load image
+    img=cv2.imread(file_path)
+    #convert color form BGR to RGB
+    img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    #display img
+    plt.imshow(img)
+    plt.show()
+
 
 #button open outlook
 button_open = tk.Button(root, command=button_open_clicked, text=configs.button_open_text,**configs.button_open_style)
